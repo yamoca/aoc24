f1093fbaac73f2c5dac58ca2ea44ac7cabb062c1
leo <leojwgulliver@gmail.com>
2024-12-01 23:05:10 +0000

    need to fix bubble sort

--- day1/part1.py
+++ day1/part1.py
@@ -7,15 +7,20 @@ sum list
 
 def bubbleSort(list):
     for i in range(len(list)-1):
-        if list[i] > list[i+1]:
-            # swap
-            temp = list[i]
-            list[i] = list[i+1]
-            list[i+1] = temp
+        for j in range(len(list)-1):
+            if list[i] > list[j+1]:
+                # swap
+                temp = list[i]
+                list[i] = list[j+1]
+                list[j+1] = temp
     
     return list
 
 
 
-testlist = [3, 1, 4, 5, 6]
-print(bubbleSort(testlist))
+list1 = [4, 2, 1, 3]
+list2 = [3, 5, 3, 9]
+list1 = bubbleSort(list1)
+list2 = bubbleSort(list2)
+print(list1)
+print(list2)
