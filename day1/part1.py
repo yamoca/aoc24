5071b711bf40c6c2674a1a8a830da9998d3f1d5f
leo <leojwgulliver@gmail.com>
2024-12-02 20:23:12 +0000

    just use python .sort() method, solve p1

--- day1/part1.py
+++ day1/part1.py
@@ -5,22 +5,37 @@ compare into new list
 sum list 
 """
 
-def bubbleSort(list):
-    for i in range(len(list)-1):
-        for j in range(len(list)-1):
-            if list[i] > list[j+1]:
-                # swap
-                temp = list[i]
-                list[i] = list[j+1]
-                list[j+1] = temp
+# def bubbleSort(list):
+#     for i in range(len(list)-1):
+#         for j in range(len(list)-1):
+#             if list[i] > list[j+1]:
+#                 # swap
+#                 temp = list[i]
+#                 list[i] = list[j+1]
+#                 list[j+1] = temp
     
-    return list
+#     return list
 
 
+list1 = []
+list2 = []
+f = open("input.txt", "r")
+for line in f.readlines():
+    parts = line.split("   ")
+    list1.insert(0, int(parts[0]))
+    list2.insert(0, int(parts[1].strip()))
 
-list1 = [4, 2, 1, 3]
-list2 = [3, 5, 3, 9]
-list1 = bubbleSort(list1)
-list2 = bubbleSort(list2)
-print(list1)
-print(list2)
+
+# list1 = [3, 4, 2, 1, 3, 3]
+# list2 = [4, 3, 5, 3, 9, 3]
+list1.sort()
+list2.sort()
+sum = 0
+for i in range(len(list1)):
+    if list1[i] > list2[i]:
+        print("difference: ", list1[i] - list2[i])
+        sum += list1[i] - list2[i]
+    else:
+        print("difference: ", list2[i] - list1[i])
+        sum += list2[i] - list1[i]
+    print(sum)
