fcbe28b88ec7ac6c64df5943a6c2d9d8dca72b4a
leo <leojwgulliver@gmail.com>
2024-12-04 20:44:26 +0000

    identify issue, must ALL be increasing/decreasing

--- day2/p1.py
+++ day2/p1.py
@@ -16,19 +16,26 @@ safe_count = 0
 for report in reports:
     unsafe = False
     if int(report[0]) == int(report[1]):
+        print(report, " first numbers equal")
         unsafe = True
         break
     elif int(report[0]) > int(report[1]): # e.g 10, 6
+        print(report, " decreasing")
         gradient = "decreasing"
     else:
         gradient = "increasing"
+        print(report, " increasing")
 
+
+    # problem where it allows decrease then increase or vice versa
     for level in range(len(report)-1):
         if gradient == "decreasing":
             if int(report[level]) - int(report[level+1]) > 3:
+                print(report, " unsafe at step 1" )
                 unsafe = True
         if gradient == "increasing":
-            if int(report[level]) - int(report[level+1]) < 3:
+            if int(report[level]) - int(report[level+1]) < -3:
+                print(report, " unsafe at step 2")
                 unsafe = True
 
     if not unsafe:
