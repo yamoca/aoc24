6178e31f3fe799837f7217eff8a2f8c5bfa2dae9
leo <leojwgulliver@gmail.com>
2024-12-05 00:06:18 +0000

    losing my mind

--- day2/p2.py
+++ day2/p2.py
@@ -6,21 +6,27 @@ if counter = 0 and unsafe, counter = 1 and remove current level, recheck by doin
 
 
 
+# reports = []
+# f = open("input.txt")
+# for line in f.readlines():
+#     report = line.strip().split(" ")
+#     reports.append(report)
+
 reports = []
-f = open("testinput.txt")
-for line in f.readlines():
-    report = line.strip().split(" ")
-    reports.append(report)
+with open("input.txt") as f:
+    for line in f:
+        report = [int(x) for x in line.strip().split(" ")]
+        reports.append(report)
 
 
 safe_count = 0
 reportnum = 0
-for report in reports:
-    reportnum += 1
-    unsafe = False
+
+def checkIsSafe(report):
+    # reportnum += 1
     if int(report[0]) == int(report[1]):
         # print(report, " first numbers equal")
-        unsafe = True
+        return False 
     elif int(report[0]) > int(report[1]): # e.g 10, 6
         # print(report, " decreasing")
         gradient = "decreasing"
@@ -30,52 +36,70 @@ for report in reports:
 
 
     ### PROBLEM: IN SOME SITUATIONS SHOULD REMOVE i, some others should remove i+1
+    ### solution: must brute force (test sublists of removing each number from list aka consider 5234, error happens at 2 3 but answer is change 5 not 2 or 3)
     ### also incorrectly saying changed things are safe (not tracking back properly?)
-    removedCounter = 0 
-    copy = []
+    # removedCounter = 0 
+    # copy = []
 
 
     levelnumber = 0 
     while levelnumber < len(report)-1:
-        print(report[levelnumber], report[levelnumber+1])
-        print()
-        print("checking with above values")
+        # print(report[levelnumber], report[levelnumber+1])
+        # print()
+        # print("checking with above values")
         if gradient == "decreasing":
             if int(report[levelnumber]) - int(report[levelnumber+1]) > 3 or int(report[levelnumber]) <= int(report[levelnumber+1]):
-                if removedCounter == 0:
-                    removedval = report.pop(levelnumber)
-                    print(report, "after removing")
-                    print("value number: ", levelnumber, "value removed: ", removedval) 
-                    removedCounter += 1
-                    print(report[levelnumber], report[levelnumber+1])
-                    if levelnumber != 0:
-                        levelnumber -=2 
-                    print(report[levelnumber], report[levelnumber+1])
-                else:
-                    unsafe = True
+                # if removedCounter == 0:
+                #     checkIsSafe(report.pop(levelnumber))
+                    # removedval = report.pop(levelnumber)
+                    # print(report, "after removing")
+                    # print("value number: ", levelnumber, "value removed: ", removedval) 
+                    # removedCounter += 1
+                    # print(report[levelnumber], report[levelnumber+1])
+                    # if levelnumber != 0:
+                    #     levelnumber -=2 
+                    # print(report[levelnumber], report[levelnumber+1])
+                # else:
+                return False
         if gradient == "increasing":
             if int(report[levelnumber]) - int(report[levelnumber+1]) < -3 or int(report[levelnumber]) >= int(report[levelnumber+1]):
-                if removedCounter == 0:
-                    removedval = report.pop(levelnumber)
-                    print(report, "after removing")
-                    print("value number: ", levelnumber, "value removed: ", removedval) 
-                    removedCounter += 1
-                    print(report[levelnumber], report[levelnumber+1])
-                    if levelnumber != 0:
-                        levelnumber -=2 
-                    print(report[levelnumber], report[levelnumber+1])
-                else:
-                    unsafe = True
+                # if removedCounter == 0:
+                #     checkIsSafe(report.pop(levelnumber))
+                #     # removedval = report.pop(levelnumber)
+                    # print(report, "after removing")
+                    # print("value number: ", levelnumber, "value removed: ", removedval) 
+                    # removedCounter += 1
+                    # print(report[levelnumber], report[levelnumber+1])
+                    # if levelnumber != 0:
+                    #     levelnumber -=2 
+                    # print(report[levelnumber], report[levelnumber+1])
+                # else:
+                return False
 
         levelnumber += 1
 
 
 
-    if not unsafe:
-        print(report, " (report number: ", reportnum, ") was safe")
-        print(copy)
-        print()
+    # print(report, " (report number: ", reportnum, ") was safe")
+    # print(copy)
+    return True
+
+
+for report in reports:
+    if checkIsSafe(report):
+        # print(report, "was safe")
         safe_count += 1
+        continue
+    for i in range(len(report)-1):
+        # print("report, ", report)
+        subreport = report[:]
+        subreport.pop(i)
+        # print("copy, ", subreport)
+        if checkIsSafe(subreport):
+            print(subreport, "is safe")
+            safe_count += 1
+            break
+
 
 
     # for level in range(len(report)-1):
