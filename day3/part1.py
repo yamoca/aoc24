a08aca79ee289afc0d4b6cfc14041058015b93b9
leo <leojwgulliver@gmail.com>
2024-12-07 15:19:35 +0000

    abandon first approach for regexs

--- day3/part1.py
+++ day3/part1.py
@@ -13,6 +13,7 @@ mul(abc,xyz)
 """
 
 testinput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+# mul(2x4)
 
 def isDigit(char):
     try:
@@ -22,4 +23,22 @@ def isDigit(char):
         return False
 
 
-print(isDigit("d"))
+# print(isDigit("d"))
+
+
+
+# while True:
+try:
+    startIndex = testinput.index("mul(", )
+    firstInputIndexStart = startIndex + 4 
+    if isDigit(testinput[firstInputIndexStart+1]): # check if character after mul( is an int
+
+
+    elif testinput[firstInputIndexStart+1] == ",": # if its not, check if its a ","
+        pass
+    else: # else, chop input string here, as its not a valid instruction
+        testinput = testinput[firstInputIndexStart:]
+
+except:
+    # break
+    print("end")
