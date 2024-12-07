8c88e3da350545d8acae20f971c8149166be0e03
leo <leojwgulliver@gmail.com>
2024-12-07 15:54:39 +0000

    finish part1 with regexs

--- day3/part1.py
+++ day3/part1.py
@@ -11,34 +11,48 @@ mul(ab,xy)
 mul(abc,xyz)
 
 """
+import re
 
 testinput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 # mul(2x4)
 
-def isDigit(char):
-    try:
-        int(char)
-        return True      
-    except:
-        return False
+sum = 0 
+
+with open("input.txt") as f:
+    data = f.readlines()
+
+for line in data:
+    matches = re.findall(r"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)", line) # use r"___" for raw string so python doesnt get annoyed at backslashes
+    print(matches)
+    for match in matches:
+        sum += int(match[0]) * int(match[1])
+
+print(sum)
+
+# def isDigit(char):
+#     try:
+#         int(char)
+#         return True      
+#     except:
+#         return False
 
 
-# print(isDigit("d"))
+# # print(isDigit("d"))
 
 
 
-# while True:
-try:
-    startIndex = testinput.index("mul(", )
-    firstInputIndexStart = startIndex + 4 
-    if isDigit(testinput[firstInputIndexStart+1]): # check if character after mul( is an int
+# # while True:
+# try:
+#     startIndex = testinput.index("mul(", )
+#     firstInputIndexStart = startIndex + 4 
+#     if isDigit(testinput[firstInputIndexStart+1]): # check if character after mul( is an int
 
 
-    elif testinput[firstInputIndexStart+1] == ",": # if its not, check if its a ","
-        pass
-    else: # else, chop input string here, as its not a valid instruction
-        testinput = testinput[firstInputIndexStart:]
+#     elif testinput[firstInputIndexStart+1] == ",": # if its not, check if its a ","
+#         pass
+#     else: # else, chop input string here, as its not a valid instruction
+#         testinput = testinput[firstInputIndexStart:]
 
-except:
-    # break
-    print("end")
+# except:
+#     # break
+#     print("end")
