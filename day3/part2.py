a24b42c5041520ba72dd3d359aa8a7d5ed7e8af9
leo <leojwgulliver@gmail.com>
2024-12-07 16:33:13 +0000

    problem was resetting do flag inbetween each line because of dumb input handling

--- day3/part2.py
+++ day3/part2.py
@@ -1,17 +1,13 @@
 import re
 
-" "
-
-
 testinput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 sum = 0 
-
+do = True 
 with open("input.txt") as f:
     data = f.readlines()
 
 for line in data:
-    do = True 
     matches = re.findall(r"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|don't\(\)|do\(\))", line) # use r"___" for raw string so python doesnt get annoyed at backslashes
     for match in matches:
         print(sum)
