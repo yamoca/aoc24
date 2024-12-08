df38f0472260020f4ab66ca8ee83ea8d965782dd
leo <leojwgulliver@gmail.com>
2024-12-08 14:27:57 +0000

    rotate wordsearch and fix regex so matches overlapping

--- day4/part1.py
+++ day4/part1.py
@@ -7,11 +7,26 @@ with open("testinput.txt") as f:
 
 
 horizonantal = []
-for line in data:
-    matches = re.findall("XMAS", line)
-    if len(matches) > 0:
-        for match in matches:
-            horizonantal.append(match)
+def count_matches(data):
+    for line in data:
+        matches = re.findall("(?=(XMAS|SAMX))", line) #(?=) is a lookahead to find overlapping e.g XMASAMX should find 2
+        if len(matches) > 0:
+            for match in matches:
+                horizonantal.append(match)
 
-print(len(horizonantal))
-print(horizonantal)
+    print(len(horizonantal))
+    print(horizonantal)
+
+
+count_matches(data)
+
+rotatedtuple = tuple(zip(*data))
+rotated = []
+for line in rotatedtuple:
+    line = ''.join(line)
+    rotated.insert(0, line)
+
+print("normal", data)
+print("rotated", rotated)
+
+count_matches(rotated)
