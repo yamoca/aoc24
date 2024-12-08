292478e6691928dcac7a1b8e2500c2019c369538
leo <leojwgulliver@gmail.com>
2024-12-08 13:03:47 +0000

    forwards search

--- day4/part1.py
+++ day4/part1.py
@@ -1,8 +1,17 @@
+import re
+
 data = []
 with open("testinput.txt") as f:
     for line in f.readlines():
         data.append(line.strip())
 
 
+horizonantal = []
+for line in data:
+    matches = re.findall("XMAS", line)
+    if len(matches) > 0:
+        for match in matches:
+            horizonantal.append(match)
 
-print(data)
+print(len(horizonantal))
+print(horizonantal)
