ace47cb67f9a0f807109dedd29fbd406d7c4445a
leo <leojwgulliver@gmail.com>
2024-12-24 23:22:02 +0000

    func to move freespace to right and file blocks to left

--- day9/part1.py
+++ day9/part1.py
@@ -1,23 +1,34 @@
-# if 
-
 testdiskmap = "2333133121414131402"
 
-
 def diskmap_to_diskblocks(diskmap):
-    diskblocks = "" 
+    diskblocks = [] 
     id = 0
     for i in range(len(diskmap)):
         if i % 2 != 0: # odd index's are free space blocks 
             block = ["." for x in range(int(diskmap[i]))]
             for num in block:
-                diskblocks += num
+                diskblocks.append(num)
         else:
             block = [id for x in range(int(diskmap[i]))]
             id += 1
             for num in block:
-                diskblocks += str(num)
+                diskblocks.append(num)
 
     return diskblocks
 
 
-print(diskmap_to_diskblocks(testdiskmap))
+testdiskblock = diskmap_to_diskblocks(testdiskmap)
+print(testdiskblock)
+
+
+def moveFreeSpace(diskblocks):
+    for i in range(len(diskblocks)-1, -1, -1): # loop backwards
+        diskblocks[diskblocks.index(".")] = diskblocks[i]
+        diskblocks[i] = "."
+    # ends up correct but with a "." in front so just remove it and put it at back
+    # (because loops through whole list, doesnt stop as soon as there numbers and "." are seperated)
+    diskblocks.pop(0)
+    diskblocks.append(".")
+    return diskblocks
+
+print(moveFreeSpace(testdiskblock))
