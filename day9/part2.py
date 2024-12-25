3202f5ccee55a3671de624e60f9633ce89e11fa4
leo <leojwgulliver@gmail.com>
2024-12-25 02:51:17 +0000

    right idea currently broken

--- day9/part2.py
+++ day9/part2.py
@@ -8,47 +8,67 @@ def diskmap_to_diskblocks(diskmap):
     for i in range(len(diskmap)):
         if i % 2 != 0: # odd index's are free space blocks 
             block = ["." for x in range(int(diskmap[i]))]
-            for num in block:
-                diskblocks.append(num)
+            if len(block) >= 1: #only append if block is not empty 
+                diskblocks.append(block)
         else:
             block = [id for x in range(int(diskmap[i]))]
             id += 1
-            for num in block:
-                diskblocks.append(num)
+            diskblocks.append(block)
 
     return diskblocks
 
 
 testdiskblock = diskmap_to_diskblocks(testdiskmap)
-
+print(testdiskblock)
 
 def moveFreeSpace(diskblocks):
-    currentChar = diskblocks[len(diskblocks)] 
     for i in range(len(diskblocks)-1, -1, -1): # loop backwards
-        if diskblocks[i] == "john":
-            pass
+        print()
+        print()
+        print()
+        print(diskblocks[i])
+        print("before changes this round")
+        print(diskblocks)
+        free_space_index = findFreeSpaceToLeft(diskblocks, i, len(diskblocks[i]))
+        if free_space_index != -1:
+            size_of_space = len(diskblocks[free_space_index])
+            diskblocks.insert(free_space_index, diskblocks[i])
+            print("inserted")
+            print(diskblocks)
+            if size_of_space == len(diskblocks[i]):
+                diskblocks.pop(i+1)
+                diskblocks.append(diskblocks.pop(free_space_index+1))
+            else:
+                consumed = diskblocks[free_space_index+1][len(diskblocks[i+1])-1:]
+                leftover = diskblocks[free_space_index+1][:len(diskblocks[i+1])-1]
+                print("leftovers", leftover)
+                print("consumed", consumed)
+                diskblocks.pop(i+1)
+                print("popped")
+                print(diskblocks)
+                diskblocks[free_space_index+1] = leftover
+                print("leftovers modified")
+                print(diskblocks)
+                diskblocks.append(consumed)
+                print("appended")
+                print(diskblocks)
+
+    return diskblocks
 
 
 
-# oriented = moveFreeSpace(testdiskblock)
-# print(oriented)
-
-def findEmptySpace(diskblocks, size):
-    first_free_space = diskblocks.index(".")
-    currentChar = "."
-    count = 0
-    for i in range(first_free_space, len(diskblocks)):
-        if count == size:
-            return first_free_space, i-1
-        elif diskblocks[i] == currentChar:
-            count += 1
-        else:
-            break
-    
 
 
-print(testdiskblock)
-print(findEmptySpace(testdiskblock, 3))
+def findFreeSpaceToLeft(diskblocks, currentIndex, size):
+    for i in range(currentIndex): # only look up to current index to avoid moving the wrong direction 
+        if len(diskblocks[i]) >= size and diskblocks[i][0] == ".": # if its starts with "." it will be empty space
+            return i
+    else:
+        return -1
+
+
+print(moveFreeSpace(testdiskblock))
+
 
 
 def calcChecksum(diskblocks):
